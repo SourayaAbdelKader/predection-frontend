1f8a0de5ecfdfe4549a4bf46d7089e6fc87bc25e
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-10 20:41:41 +0300

    JS: fetching age, gender and nationality

--- index.js
+++ index.js
@@ -1,8 +1,81 @@
+// Defining variables
 const name = document.querySelector('[name="name"]');
+let img_src = "https://dog.ceo/api/breeds/image/random";
 
 
-async function getText(file) {
-    let x = await fetch(file);
-    let y = await x.text();
-    myDisplay(y);
-  }
+const creatingTags = (text,id) => {
+    let tag = document.createElement("p");
+    tag.appendChild(text);
+    const element = document.getElementById(id);
+    element.appendChild(tag);
+}
+
+const picture = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.message);
+        console.log(typeof(data.message));
+        //let div = document.getElementById("api-pictue");
+        //img = document.createElement("img")
+        //img.setAttribute("src", JSON.stringify(data.message));
+        //img.setAttribute("alt", "Dog"); 
+        //div.appendChild(img)  
+    })
+}
+
+const userGender = (name) => {
+    fetch("https://api.genderize.io/?name="+name)
+    .then((response) => response.json())
+    .then((data) => {
+        user_gender = data.gender;
+        probability = data.probability;
+        if (user_gender) {text = document.createTextNode("Gender : " + user_gender.charAt(0).toUpperCase() + user_gender.slice(1) + " " + probability*100 + "%");}
+        else {text = document.createTextNode("Gender : No idea!")} 
+        creatingTags(text,"user-gender")  
+    })
+}
+
+const userAge = (name) => {
+    fetch("https://api.agify.io/?name="+name)
+    .then((response) => response.json())
+    .then((data) => {
+        user_age = data.age;
+        if (user_age) {text = document.createTextNode("Age : "+  user_age);}
+        else {text = document.createTextNode("Age : No idea!")};
+        creatingTags(text,"user-age");
+    })
+}
+
+const userNationality = (name) => {
+    fetch("https://api.nationalize.io/?name="+name)
+    .then((response) => response.json())
+    .then((data) => {
+        user_nationality = data.country;
+        if (user_nationality) {
+            text = document.createTextNode( "Nationality : ");
+            creatingTags(text,"user-nationality")
+            user_nationality.forEach(item => {
+                let box = document.createElement("div");
+                let tag = document.createElement("p");
+                text = document.createTextNode( item.country_id + " " + (+item.probability *100).toFixed(2) + " %");
+                tag.appendChild(text);
+                box.appendChild(tag);
+                const element = document.getElementById("user-nationality");
+                element.appendChild(box);
+            });
+        } else {text = document.createTextNode("No idea!")};
+    });
+};
+
+userGender("ahmad")
+userAge("ahmad")
+userNationality("ahmad")
+picture()
+
+
+
+
+
+
+
